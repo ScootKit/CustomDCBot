980d70a6f4be3c89df4e990272995f31ef2e097e
Simon Csaba <sc.de@gmx.de>
2020-12-24 15:37:39 +0100

    you can now disable invite-blocking in selected channels

--- modules/moderation/events/message.js
+++ modules/moderation/events/message.js
@@ -3,6 +3,7 @@ const {confDir} = require('../../../main');
 exports.run = async (client, msg) => {
     const moduleConfig = require(`${confDir}/moderation/config.json`);
     if (msg.author.bot) return;
+    if (moduleConfig['whitelisted_channels_for_invite_blocking'].includes(msg.channel.id) || moduleConfig['whitelisted_channels_for_invite_blocking'].includes(msg.channel.parentID)) return;
     if (msg.member.roles.cache.find(r => moduleConfig['moderator-roles_level2'].includes(r.id))) return;
     if (moduleConfig['block_invites']) {
         if (msg.content.includes('discord.gg/' || ' discordapp.com/invite/' || 'discord.invite/')) {
