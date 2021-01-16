1addf54a1e17425a8dca732b76084bfc67ea2e3f
Simon Csaba <sc.de@gmx.de>
2021-01-16 12:13:27 +0100

    [levels] Added function to see profile of another user

--- modules/levels/commands/profile.js
+++ modules/levels/commands/profile.js
@@ -2,11 +2,18 @@ const {embedType} = require('../../../src/functions/helpers');
 const {confDir} = require('./../../../main');
 const {MessageEmbed} = require('discord.js');
 
-module.exports.run = async function (client, msg) {
+module.exports.run = async function (client, msg, args) {
     const moduleStrings = require(`${confDir}/levels/strings.json`);
+
+    let member = msg.author;
+    if (args[0]) {
+        if (msg.mentions.members.first()) member = msg.mentions.members.first().user;
+        else member = await (await msg.guild.members.fetch(args[0])).user;
+        if (!member) return message.edit(...embedType(moduleStrings['user_not_found']));
+    }
     let user = await client.models['levels']['User'].findOne({
         where: {
-            userID: msg.author.id
+            userID: member.id
         }
     });
     if (!user) return msg.channel.send(...embedType(moduleStrings['not-found']));
@@ -14,19 +21,18 @@ module.exports.run = async function (client, msg) {
     const embed = new MessageEmbed()
         .setFooter(client.strings.footer)
         .setColor('GREEN')
-        .setThumbnail(msg.author.avatarURL())
-        .setTitle(`${moduleStrings.embed.title} ${msg.author.tag}`)
+        .setThumbnail(member.avatarURL())
+        .setTitle(`${moduleStrings.embed.title} ${member.tag}`)
         .setDescription(moduleStrings.embed.description)
         .addField(moduleStrings.embed.messages, user.messages, true)
         .addField(moduleStrings.embed.xp, `${user.xp}/${nextLevelXp}`, true)
         .addField(moduleStrings.embed.level, user.level, true);
     await msg.channel.send(embed);
-
 };
 
 module.exports.help = {
     'name': 'profile',
-    'description': 'Your Level-Profile',
+    'description': 'Shows the Level-Profile of you or another user',
     'module': 'levels',
     'aliases': ['p', 'profile']
 };
