3be10bef1ed278d0594395709983c75746396026
Simon Csaba <sc.de@gmx.de>
2021-02-03 10:00:08 +0100

    Added module "ping-on-vc-join"

---
+++ modules/ping-on-vc-join/events/voiceStateUpdate.js
@@ -0,0 +1,29 @@
+const {embedType} = require('../../../src/functions/helpers');
+
+exports.run = async (client, oldState, newState) => {
+    const moduleConfig = require(`${client.configDir}/ping-on-vc-join/config.json`)
+
+    if (!newState.channelID) return;
+    if (!moduleConfig.channels.includes(newState.channelID)) return;
+
+    const memberChannel = newState.guild.channels.cache.get(newState.channelID);
+    if (!memberChannel) return;
+
+    const member = await newState.guild.members.fetch(newState.id);
+    if (!member) return;
+
+    const notifyChannel = newState.guild.channels.cache.get(moduleConfig['notify_channel_id']);
+    if (!notifyChannel) return console.error(`[Module: ping-on-vc-join] Notify channel not found`)
+
+    notifyChannel.send(...embedType(moduleConfig['message'], {
+        '%vc%': memberChannel.name,
+        '%tag%': member.user.tag,
+        '%mention%': `<@${member.user.id}>`
+    }))
+
+    if (moduleConfig['send_pn_to_member']) {
+        await member.send(...embedType(moduleConfig['pn_message'], {
+            '%vc%': memberChannel.name,
+        })).catch(e => {console.error(`[Module: ping-on-vc-join] Could not send PN to ${member.user.id}`)})
+    }
+}
