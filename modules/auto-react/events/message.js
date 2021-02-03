91541d7148ea4904d4349e8a8c653ed734f5cc20
Simon Csaba <sc.de@gmx.de>
2021-02-03 09:04:33 +0100

    [auto-react] React on Ping™️

--- modules/auto-react/events/message.js
+++ modules/auto-react/events/message.js
@@ -1,7 +1,22 @@
 exports.run = async (client, msg) => {
-    const moduleConfig = require(`${client.configDir}/auto-react/config.json`);
+    checkChannel(msg);
+    checkMembers(msg);
+};
+
+function checkMembers(msg) {
+    const moduleConfig = require(`${msg.client.configDir}/auto-react/config.json`);
+    msg.mentions.members.forEach(m => {
+        if (!moduleConfig.members[m.id]) return;
+        moduleConfig.members[m.id].split('|').forEach(emoji => {
+            msg.react(emoji);
+        });
+    });
+}
+
+function checkChannel(msg) {
+    const moduleConfig = require(`${msg.client.configDir}/auto-react/config.json`);
     if (!moduleConfig.channels[msg.channel.id]) return;
     moduleConfig.channels[msg.channel.id].split('|').forEach(emoji => {
         msg.react(emoji);
     });
-};
+}
