3e2ea36581489f04dfb8cf763f8439f9ec9ea2b9
Simon Csaba <sc.de@gmx.de>
2021-03-20 08:36:02 +0100

    [moderation] Fixed bug with permissions

--- modules/moderation/commands/clear.js
+++ modules/moderation/commands/clear.js
@@ -1,18 +1,16 @@
 const {embedType} = require('../../../src/functions/helpers');
 const {confDir} = require('./../../../main');
-const {MessageEmbed} = require('discord.js');
-const {moderationAction} = require('../moderationActions');
 
 module.exports.run = async function (client, msg, args) {
     const moduleConfig = require(`${confDir}/moderation/config.json`);
     const moduleStrings = require(`${confDir}/moderation/strings.json`);
-    if (!msg.member.roles.cache.find(r => moduleConfig['moderator-roles_level3'].includes(r.id))) msg.edit(...embedType(moduleStrings['no_permissions'], {
+    if (!msg.member.roles.cache.find(r => moduleConfig['moderator-roles_level3'].includes(r.id) || moduleConfig['moderator-roles_level4'].includes(r.id))) return message.edit(...embedType(moduleStrings['no_permissions'], {
         '%required_level%': 3
     }));
     await msg.channel.bulkDelete((args[0]) ? parseInt(args[0]) : 50, true).then(m => {
         msg.channel.send(`Successfully deleted ${m.size} messages. NOTE: Messages older than 14 days can not be deleted.`);
     }).catch(() => {
-        msg.channel.send('An error occurred. You can only delete 100 messages at one.');
+        msg.channel.send('An error occurred. You can only delete 100 messages at once.');
     });
 };
 
