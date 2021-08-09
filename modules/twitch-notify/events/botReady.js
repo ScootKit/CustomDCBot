773b6d82e926d94ee1b76d7fb888be37babb799f
jot737 <joeteute@gmail.com>
2021-08-09 19:33:54 +0200

    Started usind Twitch Package

--- modules/twitch-notify/events/botReady.js
+++ modules/twitch-notify/events/botReady.js
@@ -1,75 +1,41 @@
 const {confDir} = require('../../../main');
-const c = require('centra')
+const { TwitchApiClient } = require('twitch');
+const { TwitchClientCredentialsAuthProvider } = require('twitch-auth');
+
+const clientId = '84mlld0q4slrppp5a3z2s2eb82l4pf';
+const clientSecret = 'jbzdrayiia45ewssgwcndw5gg1b9vo';
+const authProvider = new TwitchClientCredentialsAuthProvider(clientId, clientSecret);
+const ttvApiClient = new TwitchApiClient({ authProvider });
+
 live = false;
-
-/*To do
-
-Use centra
-Check if there are any Bugs*/
-
-async function getToken(ClientID, ClientSecret) {
-  /*var options = {
-    url: 'https://id.twitch.tv/oauth2/token?client_id=' +ClientID+'&client_secret=' +ClientSecret+ '&grant_type=client_credentials',
-    method: 'POST'
-};
-      
-  request(options, function (error, response, body) {
-    var body2 = JSON.parse(body);
-    return body2['access_token']
-  });*/
-};
-
-async function checkLive (streamer, clientID, token) {
-  /*var options = {
-    url: 'https://api.twitch.tv/helix/streams?user_login='+streamer.toLowerCase(),
-    method: 'GET',
-    headers: {
-      'Client-ID': clientID,
-      'Authorization': 'Bearer ' + token
-  } 
-};
-
-  request(options,(error, response, body) => {
-    return body
-  });*/
-};
-
-async function replacer(msg, username, game) {
-  msg = msg.split('%streamer%').join(username)
-			.split('%game%').join(game)
-			.split('%url%').join('https://twitch.tv/' + username.toLowerCase());
-  return msg
-};
-
-async function sendMSG(client) {
-  const moduleConf = require(`${confDir}/twitch-notify/config.json`);
-  var token = await getToken(moduleConf['Twitch-ClientID'], moduleConf['ClientSecret']);
-  var body = await checkLive(moduleConf['streamer'], moduleConf['Twitch-ClientID'], token);
-  var bodyJSON = JSON.parse(body);
-  var msg = await replacer(moduleConf['live-message'], bodyJSON['data'][0]['user_name'], bodyJSON['data'][0]['game_name']);
-  if ((bodyJSON['data'][0]['type'] === "live")) {
-    if (live) {
-      //pass
-    } else {
-      live = true
-      //send the Message
-      const channel = await client.channels.fetch(moduleConf['live-message-channel']).catch(e => {
-      });
-      if (!channel) return console.error(`[twitch-notifications] Could not find channel with id ${moduleConfig['live-message-channel']}`);
-      channel.send(msg);
-    };
-  } else {
-    if (live) {
-      live = false;
-    } else {
-      //pass
-    };
+async function twitch_notify(client) {
+  function replacer(msg, username, game) {
+    msg = msg.split('%streamer%').join(username)
+        .split('%game%').join(game)
+        .split('%url%').join('https://twitch.tv/' + username.toLowerCase());
+    return msg
   };
+
+  function isStreamLive(userName) {
+    const user = ttvApiClient.helix.users.getUserByName(userName.toLowerCase());
+    if (!user) {
+      return false;
+    }
+    return user.getStream() !== null;
+  };
+
+  isStreamLive(confDir['streamer']).then(function (stream) {
+    if(stream) {
+      
+    }
+  }, function (err) {
+    console.error(err)
+  })
 };
 
 exports.run = async (client) => {
-  await sendMSG(client);
+  await twitch_notify(client);
   setInterval(() => {
-    sendMSG(client);
+    twitch_notify(client);
   }, 60000);
-}};
+};
