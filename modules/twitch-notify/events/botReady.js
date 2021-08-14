beeca14ea9beb6e3824e3d6afa4be2de283f51f7
jot737 <joeteute@gmail.com>
2021-08-14 19:46:17 +0200

    Made some Changes on the module twitch-notify

--- modules/twitch-notify/events/botReady.js
+++ modules/twitch-notify/events/botReady.js
@@ -1,52 +1,62 @@
 const {confDir} = require('../../../main');
 const config = require(`${confDir}/twitch-notify/config.json`);
+const {embedType} = require('../../../src/functions/helpers');
 
-const { ApiClient } = require('twitch');
-const { ClientCredentialsAuthProvider } = require('twitch-auth');
+const {ApiClient} = require('twitch');
+const {ClientCredentialsAuthProvider} = require('twitch-auth');
 
-const ClientID = config['Twitch-ClientID'];
-const ClientSecret = config['ClientSecret'];
+const ClientID = config['twitchClientID'];
+const ClientSecret = config['clientSecret'];
 const authProvider = new ClientCredentialsAuthProvider(ClientID, ClientSecret);
-const apiClient = new ApiClient({ authProvider });
+const apiClient = new ApiClient({authProvider});
 
-let live = false;
-
-function twitch_notify(client) {
-  function replacer(msg, username, game) {
-    msg = msg.split('%streamer%').join(username)
-        .split('%game%').join(game)
-        .split('%url%').join('https://twitch.tv/' + username.toLowerCase());
-    return msg
-  };
-
-  function sendMSG(username, game) {
-    const channel = client.channels.cache.get(config['live-message-channel'])
-    if (!channel) return console.error(`[twitch-notify] Could not find channel with id ${config['live-message-channel']}`);
-    msg = replacer(config['live-message'], username, game);
-    channel.send(msg);
+function twitchNotify(client) {
+  function sendMsg(username, game, thumbnailUrl, channelID) {
+    const channel = client.channels.cache.get(channelID)
+    if (!channel) return console.error(`[twitch-notify] Could not find channel with id ${channelID}`);
+    channel.send(...embedType(config['liveMessage'], {
+      '%streamer%': username,
+      '%game%': game,
+      '%url%': `https://twitch.tv/${username.toLowerCase()}`,
+      '%thumbnailUrl': thumbnailUrl
+    }));
   }
 
   async function isStreamLive(userName) {
     const user = await apiClient.helix.users.getUserByName(userName.toLowerCase());
-    if (!user) {
-      return false;
-    }
+    if (!user) return false;
     return await user.getStream();
   };
+  
+  config['streamers'].forEach(start)
+  
+  function start(value, index) {
+    let streamer = await client.models['twitch-notify']['streamer'].findOne({
+      where: {
+          id: value.toLowerCase()
+      }
+  });
+    const stream = await isStreamLive(value)
 
-  isStreamLive(config['streamer']).then(function (stream) {
-    if(stream !== null && !live) {
-      live = true;
-      sendMSG(stream.userDisplayName, stream.gameName)
+    if(stream !== null && stream.startDate !== streamer.startedAt) {
+      streamer.startedAt = stream.startDate;
+      streamer.save()
+      sendMsg(stream.userDisplayName, stream.gameName, stream.thumbnailUrl, config['liveMessageChannels'][index])
+    } else if(stream !== null && !streamer) {
+      client.models['twitch-notify']['streamer'].create({
+        id: value,
+        startedAt: stream.startDate
+      });
+      sendMsg(stream.userDisplayName, stream.gameName, stream.thumbnailUrl, config['liveMessageChannels'][index])
     }
-  }, function (err) {
-    console.error(err)
-  })
+  };
 };
 
 exports.run = async (client) => {
-  await twitch_notify(client);
+  await twitchNotify(client);
+  if(config['interval'] > 60) return console.error(`[twitch-notify] The value of the interval must be equal or higher than 60`);
+  interval = config['interval'] * 1000;
   setInterval(() => {
-    twitch_notify(client);
-  }, 60000);
+    twitchNotify(client);
+  }, interval);
 };
---
+++ modules/twitch-notify/models/streamer.js
@@ -0,0 +1,22 @@
+const {DataTypes, Model} = require('sequelize');
+
+module.exports = class streamer extends Model {
+    static init(sequelize) {
+        return super.init({
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true
+            },
+            startedAt: DataTypes.DATE
+        }, {
+            tableName: 'twitch_streamers',
+            timestamps: true,
+            sequelize
+        });
+    }
+};
+
+module.exports.config = {
+    'name': 'streamer',
+    'module': 'twitch-notify'
+};
