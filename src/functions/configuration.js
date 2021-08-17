4ac8483a1a82608183869c0254377293af62bb80
Simon Csaba <mail@scderox.de>
2021-08-17 21:30:14 +0200

    what the fuck happened there, i was probably brain-dead or something

--- src/functions/configuration.js
+++ src/functions/configuration.js
@@ -11,17 +11,17 @@ const {logger} = require('../../main');
  */
 async function loadAllConfigs(client, moduleConf) {
     logger.info('Checking configs...');
-    return new Promise(async resolve => {
+    return new Promise(async (resolve, reject) => {
         await fs.readdir(`${__dirname}/../../config-generator/`, async (err, files) => {
             await asyncForEach(files, async f => {
-                await checkBuildInConfig(f);
+                await checkBuildInConfig(f).catch((reason) => reject(reason))
             });
             await fs.readdir(`${__dirname}/../../modules/`, async (err, files) => {
                 let needOverwrite = false;
                 await asyncForEach(files, async f => {
                     if (moduleConf[f]) {
                         if (client.modules[f]['config']['on-checked-config-event']) await checkModuleConfig(f, require(`./modules/${f}/${client.modules[f]['config']['on-checked-config-event']}`));
-                        else await checkModuleConfig(f);
+                        else await checkModuleConfig(f).catch((reason) => {reject(reason);})
                     } else if (typeof moduleConf[f] === 'undefined') needOverwrite = true;
                 });
                 if (needOverwrite) await generateModulesConfOverwrite(moduleConf, files);
@@ -32,38 +32,39 @@ async function loadAllConfigs(client, moduleConf) {
     });
 }
 
-function checkModuleConfig (moduleName, afterCheckEventFile = null) {
-    return new Promise(resolve => {
+async function checkModuleConfig (moduleName, afterCheckEventFile = null) {
+    return new Promise(async (resolve, reject) => {
         const {client} = require('../../main');
         const moduleConf = require(`../../modules/${moduleName}/module.json`);
         if (!moduleConf['config-example-files']) return resolve();
-        moduleConf['config-example-files'].forEach(v => {
+        for (const v of moduleConf['config-example-files']) {
             let exampleFile;
             try {
                 exampleFile = require(`../../modules/${moduleName}/${v}`);
             } catch (e) {
                 logger.error(`Not found config example file: ${moduleName}/${v}`);
-                process.exit(1);
+                return reject(`Not found config example file: ${moduleName}/${v}`)
             }
             if (!exampleFile) return;
             let config = exampleFile.configElements ? [] : {};
             let ow = false;
             try {
-                config = require(`${client.configDir}/${moduleName}/${exampleFile.filename}`);
+                config = jsonfile.readFileSync(`${client.configDir}/${moduleName}/${exampleFile.filename}`);
             } catch (e) {
                 logger.info(`Config ${moduleName}/${exampleFile.filename} does not exist - I'm going to create it - stand by...`);
                 ow = true;
             }
+            console.log(config)
             if (exampleFile.configElements) {
-                exampleFile.content.forEach(async field => {
-                    await asyncForEach(config, async (element) => {
-                        config = await checkField(field, element);
-                    });
-                });
+                for (const field of exampleFile.content) {
+                    for (const element of config) {
+                        await checkField(field, element)
+                    }
+                }
             } else {
-                exampleFile.content.forEach(async field => {
-                    config = await checkField(field, config);
-                });
+                for (const field of exampleFile.content) {
+                    await checkField(field, config);
+                }
             }
 
             async function checkField(field, configElement) {
@@ -71,13 +72,13 @@ function checkModuleConfig (moduleName, afterCheckEventFile = null) {
                 if (typeof configElement[field.field_name] === 'undefined') return configElement[field.field_name] = field.default;
                 if (!await checkType(field.type, configElement[field.field_name], field.content, field.allowEmbed)) {
                     logger.error(`An error occurred while checking the content of field ${field.field_name} in ${moduleName}/${exampleFile.filename}`);
-                    process.exit(1); // ToDo Only disable plugin not stop bot
+                    return reject(`An error occurred while checking the content of field ${field.field_name} in ${moduleName}/${exampleFile.filename}`)
                 }
                 if (field.disableKeyEdits) {
                     for (const content in configElement[field.field_name]) {
                         if (!field.default[content]) {
                             logger.error(`Error with ${content} in ${field.field_name} in ${moduleName}/${exampleFile.filename}: Unexpected index ${content}`);
-                            process.exit(1); // ToDo Only disable plugin not stop bot
+                            return reject(`Error with ${content} in ${field.field_name} in ${moduleName}/${exampleFile.filename}: Unexpected index ${content}`)
                         }
                     }
                 }
@@ -86,7 +87,7 @@ function checkModuleConfig (moduleName, afterCheckEventFile = null) {
 
             if (ow) {
                 if (!fs.existsSync(`${client.configDir}/${moduleName}`)) fs.mkdirSync(`${client.configDir}/${moduleName}`)
-                jsonfile.writeFileSync(`${client.configDir}/${moduleName}/${exampleFile.filename}`, config, {spaces: 2}, (err=> {
+                jsonfile.writeFileSync(`${client.configDir}/${moduleName}/${exampleFile.filename}`, config, {spaces: 2}, (err => {
                     if (err) {
                         logger.error(`An error occurred while saving ${moduleName}/${exampleFile.filename}: ${err}`);
                     } else {
@@ -98,13 +99,13 @@ function checkModuleConfig (moduleName, afterCheckEventFile = null) {
                 resolve();
             }
             client.configurations[moduleName][exampleFile.filename.split('.json').join('')] = config;
-        });
+        }
         if (afterCheckEventFile) require(`../../modules/${moduleName}/${afterCheckEventFile}`).afterCheckEvent(config);
     });
-};
+}
 
 async function checkBuildInConfig (configName) {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         const {client} = require('../../main');
         const exampleFile = require(`../../config-generator/${configName}`);
         if (!exampleFile) return;
@@ -121,13 +122,13 @@ async function checkBuildInConfig (configName) {
             if (!config[field.field_name]) return config[field.field_name] = field.default;
             if (!await checkType(field.type, config[field.field_name], field.content, field.allowEmbed)) {
                 logger.error(`An error occurred while checking the content of field ${field.field_name} in config/${configName}`);
-                process.exit(1);
+                return reject(`An error occurred while checking the content of field ${field.field_name} in ${exampleFile.filename}`)
             }
             if (field.disableKeyEdits) {
                 for (const content in config[field.field_name]) {
                     if (!field.default[content]) {
                         logger.error(`Error with ${content} in ${field.field_name} in config/${configName}: Unexpected index ${content}`);
-                        process.exit(1);
+                        return reject(`An error occurred while checking the content of field ${field.field_name} in ${exampleFile.filename}`)
                     }
                 }
             }
@@ -231,7 +232,8 @@ module.exports.reloadConfig = async function(client) {
     client.logger.info('Reloading all configurations...')
     client.botReadyAt = null;
     client.emit('configReload')
-    await loadAllConfigs(client, client.modules);
+    console.log(client.moduleConf)
+    await loadAllConfigs(client, client.moduleConf);
     client.botReadyAt = new Date();
     client.emit('botReady')
     client.logger.info('Configuration reloaded successfully')
