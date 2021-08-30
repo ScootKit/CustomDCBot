ee8a5d23e01e45b45b3d5ebc70eac0cdee728e5e
Simon Csaba <mail@scderox.de>
2021-08-30 14:42:59 +0200

    feat: Added compareArrays to helpers.js

--- src/functions/helpers.js
+++ src/functions/helpers.js
@@ -174,3 +174,24 @@ async function sendMultipleSiteButtonMessage(channel, sites = [], allowedUserIDs
 }
 
 module.exports.sendMultipleSiteButtonMessage = sendMultipleSiteButtonMessage;
+
+/**
+ * Compares two arrays
+ * @param {Array} array1 First array
+ * @param {Array} array2 Second array
+ * @returns {boolean} Wherever the arrays are the same
+ */
+function compareArrays(array1, array2) {
+    if (array1.length !== array2.length) return false;
+
+    for (let i = 0, l = array1.length; i < l; i++) {
+        if (array1[i] instanceof Array && array2[i] instanceof Array) {
+            if (!compareArrays(array1[i], array2[i])) return false;
+        } else if (array1[i] !== array2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports.compareArrays = compareArrays;
