ffc906dcd9aecd31c6761fba6f0c22fbf582d265
Simon Csaba <mail@scderox.de>
2021-09-05 22:50:54 +0200

    tests: """fixed""" tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,7 +51,7 @@ module.exports = {
             'error',
             'last'
         ],
-        'complexity': 'error',
+        'complexity': ['error', 25],
         'computed-property-spacing': [
             'error',
             'never'
