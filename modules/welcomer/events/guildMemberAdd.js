93483de01fc274e61dc9fd3f685ecfa2e41ae43f
Simon <mail@scderox.de>
2021-09-12 13:38:10 +0200

    feat: added welcomer module Closes #30

---
+++ modules/welcomer/events/guildMemberAdd.js
@@ -0,0 +1,40 @@
+const {randomElementFromArray, embedType, formatDate} = require('../../../src/functions/helpers');
+
+module.exports.run = async function (client, guildMember) {
+    if (!client.botReadyAt) return;
+    if (guildMember.guild.id !== client.guild.id) return;
+    const moduleConfig = client.configurations['welcomer']['config'];
+    if (guildMember.user.bot && moduleConfig['not-send-messages-if-member-is-bot']) return;
+
+    const moduleChannels = client.configurations['welcomer']['channels'];
+
+    if (moduleConfig['give-roles-on-join'].length !== 0) {
+        await guildMember.roles.add(moduleConfig['give-roles-on-join']);
+    }
+
+    for (const channelConfig of moduleChannels.filter(c => c.type === 'join')) {
+        const channel = await guildMember.guild.channels.fetch(channelConfig.channelID).catch(() => {
+        });
+        if (!channel) {
+            client.logger.error(`[welcomer] Channel not found: ${channelConfig.channelID}`);
+            continue;
+        }
+        let message;
+        if (channelConfig.randomMessages) {
+            message = (randomElementFromArray(client.configurations['welcomer']['random-messages'].filter(m => m.type === 'join')) || {}).message;
+        }
+        if (!message) message = channelConfig.message;
+
+        await channel.send(embedType(message || 'Message not found',
+            {
+                '%mention%': guildMember.toString(),
+                '%servername%': guildMember.guild.name,
+                '%tag%': guildMember.user.tag,
+                '%guildUserCount%': (await client.guild.members.fetch()).size,
+                '%guildMemberCount%': (await client.guild.members.fetch()).filter(m => !m.user.bot).size,
+                '%memberProfilePictureUrl%': guildMember.user.avatarURL(),
+                '%createdAt%': formatDate(guildMember.user.createdAt)
+            }
+        ));
+    }
+};
---
+++ modules/welcomer/events/guildMemberRemove.js
@@ -0,0 +1,35 @@
+const {randomElementFromArray, embedType, formatDate} = require('../../../src/functions/helpers');
+
+module.exports.run = async function (client, guildMember) {
+    if (!client.botReadyAt) return;
+    if (guildMember.guild.id !== client.guild.id) return;
+    const moduleConfig = client.configurations['welcomer']['config'];
+    if (guildMember.user.bot && moduleConfig['not-send-messages-if-member-is-bot']) return;
+
+    const moduleChannels = client.configurations['welcomer']['channels'];
+
+    for (const channelConfig of moduleChannels.filter(c => c.type === 'leave')) {
+        const channel = await guildMember.guild.channels.fetch(channelConfig.channelID).catch(() => {
+        });
+        if (!channel) {
+            client.logger.error(`[welcomer] Channel not found: ${channelConfig.channelID}`);
+            continue;
+        }
+
+        let message;
+        if (channelConfig.randomMessages) {
+            message = (randomElementFromArray(client.configurations['welcomer']['random-messages'].filter(m => m.type === 'leave')) || {}).message;
+        }
+        if (!message) message = channelConfig.message;
+
+        await channel.send(embedType(message || 'Message not found',
+            {
+                '%tag%': guildMember.user.tag,
+                '%memberProfilePictureUrl%': guildMember.user.avatarURL({dynamic: true}),
+                '%joinedAt%': formatDate(guildMember.joinedAt),
+                '%guildUserCount%': (await client.guild.members.fetch()).size,
+                '%guildMemberCount%': (await client.guild.members.fetch()).filter(m => !m.user.bot).size
+            }
+        ));
+    }
+};
