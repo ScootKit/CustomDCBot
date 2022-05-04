b0989329d237b5eb6725f69b936884c7cac6b798
hfgd <46094961+hfgd123@users.noreply.github.com>
2022-05-04 22:21:09 +0200

    Created module status-roles (#59)
    * Created module status-roles
    
    * This moment when ESLint just hates you
    
    * Fixed changes suggested by SCDerox (now checking if state, removed case sensitivity problem, checking if user on guild, oldPresence error, typos) for the for I will commit again
    
    * Made for loop better readable
    
    * removed exactly one space...
    
    * Now using separate removeRoles() function and added lines 22-23
    
    * Added comment for removeRoles()

---
+++ modules/status-roles/events/presenceUpdate.js
@@ -0,0 +1,36 @@
+const {localize} = require('../../../src/functions/localize');
+
+module.exports.run = async function (client, oldPresence, newPresence) {
+
+    if (!client.botReadyAt) return;
+    if (newPresence.member.guild.id !== client.guildID) return;
+    const moduleConfig = client.configurations['status-roles']['config'];
+    const roles = moduleConfig.roles;
+    const status = moduleConfig.words;
+    const member = newPresence.member;
+
+    if (newPresence.activities.length > 0) {
+        if (newPresence.activities[0].state) {
+            if (status.some(word => newPresence.activities[0].state.toLowerCase().includes(word.toLowerCase()))) {
+                return member.roles.add(roles, localize('status-role', 'fulfilled'));
+            } else {
+                removeRoles();
+            }
+        } else {
+            removeRoles();
+        }
+    } else {
+        removeRoles();
+    }
+
+    /**
+     * Removes the roles of an user who no longer fulfills the criteria
+     */
+    function removeRoles() {
+        for (let i = 0; i < roles.length; i++) {
+            if (member.roles.cache.has(roles[i])) {
+                member.roles.remove(roles[i], localize('status-role', 'not-fulfilled'));
+            }
+        }
+    }
+};
