608f4812e49bb9da58df750cb530ec719e85b2c6
Simon <mail@scderox.de>
2022-05-26 20:16:53 +0200

    feat(auto-react): support for category-reactions was available on SCNX for a long time, forgot to update it here, sorry

--- modules/auto-react/events/messageCreate.js
+++ modules/auto-react/events/messageCreate.js
@@ -1,8 +1,9 @@
 module.exports.run = async (client, msg) => {
     if (!client.botReadyAt) return;
-    if (msg.interaction || msg.system) return;
+    if (msg.interaction || msg.system || !msg.guild || msg.guild.id !== client.config.guildID) return;
     await checkChannel(msg);
     await checkMembers(msg);
+    await checkCategory(msg);
     await checkAuthor(msg);
 };
 
@@ -35,6 +36,21 @@ async function checkChannel(msg) {
     const moduleConfig = msg.client.configurations['auto-react']['config'];
     if (!moduleConfig.channels[msg.channel.id]) return;
     moduleConfig.channels[msg.channel.id].split('|').forEach(emoji => {
+        msg.react(emoji).catch(() => {
+        });
+    });
+}
+
+/**
+ * Checks if a message need reactions (and reacts if needed) because it was send in a configured category
+ * @private
+ * @param msg [Message](https://discord.js.org/#/docs/main/stable/class/Message)
+ * @returns {Promise<void>}
+ */
+async function checkCategory(msg) {
+    const moduleConfig = msg.client.configurations['auto-react']['config'];
+    if (!moduleConfig.categories[msg.channel.parentId]) return;
+    moduleConfig.categories[msg.channel.parentId].split('|').forEach(emoji => {
         msg.react(emoji).catch(() => {
         });
     });
