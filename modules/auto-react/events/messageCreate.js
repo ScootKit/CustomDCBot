3e26e2a20e51c3f049bfd43076c68593d2f24152
hfgd <jan.lunz@web.de>
2022-05-26 16:40:30 +0200

    Added Auto react for message autors

--- modules/auto-react/events/messageCreate.js
+++ modules/auto-react/events/messageCreate.js
@@ -3,6 +3,7 @@ module.exports.run = async (client, msg) => {
     if (msg.interaction || msg.system) return;
     await checkChannel(msg);
     await checkMembers(msg);
+    await checkAuthor(msg);
 };
 
 /**
@@ -34,6 +35,21 @@ async function checkChannel(msg) {
     const moduleConfig = msg.client.configurations['auto-react']['config'];
     if (!moduleConfig.channels[msg.channel.id]) return;
     moduleConfig.channels[msg.channel.id].split('|').forEach(emoji => {
+        msg.react(emoji).catch(() => {
+        });
+    });
+}
+
+/**
+ * Checks if a message need reactions (and reacts if needed) because it was send in a configured channel
+ * @private
+ * @param msg [Message](https://discord.js.org/#/docs/main/stable/class/Message)
+ * @returns {Promise<void>}
+ */
+async function checkAuthor(msg) {
+    const moduleConfig = msg.client.configurations['auto-react']['config'];
+    if (!moduleConfig.authors[msg.member.id]) return;
+    moduleConfig.authors[msg.member.id].split('|').forEach(emoji => {
         msg.react(emoji).catch(() => {
         });
     });
