03f5b8144bfdcc3693833b62b29a5b16a4aab0bb
hfgd <46094961+hfgd123@users.noreply.github.com>
2022-05-29 19:47:40 +0200

    Update messageCreate.js

--- modules/auto-react/events/messageCreate.js
+++ modules/auto-react/events/messageCreate.js
@@ -27,6 +27,23 @@ async function checkMembers(msg) {
 }
 
 /**
+ * Checks for member pings in a message and replys with the configured message
+ * @private
+ * @param msg
+ * @returns {Promise<void>}
+ */
+async function checkMembersReply(msg) {
+    const moduleConfig = msg.client.configurations['auto-react']['config'];
+    if (!msg.mentions.members) return;
+    msg.mentions.members.forEach(m => {
+        if (moduleConfig.membersReply[m.id]) {
+            msg.reply(moduleConfig.membersReply[m.id]).catch(() => {
+            });
+        }
+    });
+}
+
+/**
  * Checks if a message need reactions (and reacts if needed) because it was send in a configured channel
  * @private
  * @param msg [Message](https://discord.js.org/#/docs/main/stable/class/Message)
