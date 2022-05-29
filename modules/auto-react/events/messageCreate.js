e2e63fc8581e5d2c9c1480ab05bda0e7f2ba6e9f
hfgd <jan.lunz@web.de>
2022-05-29 17:12:29 +0200

    undid changes that somehow were in the commit but shouldn't have been

--- modules/auto-react/events/messageCreate.js
+++ modules/auto-react/events/messageCreate.js
@@ -1,14 +1,14 @@
 module.exports.run = async (client, msg) => {
     if (!client.botReadyAt) return;
-    if (msg.interaction || msg.system) return;
+    if (msg.interaction || msg.system || !msg.guild || msg.guild.id !== client.config.guildID) return;
     await checkChannel(msg);
     await checkMembers(msg);
+    await checkCategory(msg);
     await checkAuthor(msg);
-    await checkMembersReply(msg);
 };
 
 /**
- * Checks for member pings in a message and reacts with the configured emotes
+ * Checks for member pings on a message and reacts with the configured emotes
  * @private
  * @param msg [Message](https://discord.js.org/#/docs/main/stable/class/Message)
  * @returns {Promise<void>}
@@ -21,23 +21,6 @@ async function checkMembers(msg) {
             moduleConfig.members[m.id].split('|').forEach(emoji => {
                 msg.react(emoji).catch(() => {
                 });
-            });
-        }
-    });
-}
-
-/**
- * Checks for member pings in a message and replys with the configured message
- * @private
- * @param msg
- * @returns {Promise<void>}
- */
-async function checkMembersReply(msg) {
-    const moduleConfig = msg.client.configurations['auto-react']['config'];
-    if (!msg.mentions.members) return;
-    msg.mentions.members.forEach(m => {
-        if (moduleConfig.membersReply[m.id]) {
-            msg.reply(moduleConfig.membersReply[m.id]).catch(() => {
             });
         }
     });
@@ -59,7 +42,22 @@ async function checkChannel(msg) {
 }
 
 /**
- * Checks if a message need reactions (and reacts if needed) because it was send by a configured user
+ * Checks if a message need reactions (and reacts if needed) because it was send in a configured category
+ * @private
+ * @param msg [Message](https://discord.js.org/#/docs/main/stable/class/Message)
+ * @returns {Promise<void>}
+ */
+async function checkCategory(msg) {
+    const moduleConfig = msg.client.configurations['auto-react']['config'];
+    if (!moduleConfig.categories[msg.channel.parentId]) return;
+    moduleConfig.categories[msg.channel.parentId].split('|').forEach(emoji => {
+        msg.react(emoji).catch(() => {
+        });
+    });
+}
+
+/**
+ * Checks if a message need reactions (and reacts if needed) because it was send in a configured channel
  * @private
  * @param msg [Message](https://discord.js.org/#/docs/main/stable/class/Message)
  * @returns {Promise<void>}
