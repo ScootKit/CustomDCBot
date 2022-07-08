4192bcd477b3117e3f195d2a297c2a78812b3fa0
Simon <mail@scderox.de>
2022-07-08 17:41:56 +0200

    fix(core): incorrect formatting

--- main.js
+++ main.js
@@ -106,7 +106,7 @@ const commands = [];
 db.authenticate().then(async () => {
     if (config.timezone !== process.env.TZ) {
         process.env.TZ = config.timezone;
-        logger.info(`Successfully set timezone to ${config.timezone}. The time is ${new Date().toLocaleString(client.locale)}.`)
+        logger.info(`Successfully set timezone to ${config.timezone}. The time is ${new Date().toLocaleString(client.locale)}.`);
     }
     if (scnxSetup) client.scnxHost = client.config.scnxHostOverwirde || 'https://scnx.app';
     await loadModelsInDir('/src/models');
