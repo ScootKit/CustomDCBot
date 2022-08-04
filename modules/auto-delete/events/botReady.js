880ad82d9a5f1d178a3c32f21e439f891c265b55
Torben Probst <torben.probst@caritas-altenoythe.de>
2022-08-04 15:56:48 +0200

    added filtering of channels (no duplicates in config); Added overwriting of voice-channels config

--- modules/auto-delete/events/botReady.js
+++ modules/auto-delete/events/botReady.js
@@ -2,7 +2,16 @@ const {localize} = require('../../../src/functions/localize');
 module.exports.run = async function (client) {
     const channels = client.configurations['auto-delete']['channels'];
     const voiceChannels = client.configurations['auto-delete']['voice-channels'];
+
+    const uniqueConfigChannels = findUniqueChannels(channels);
     const uniqueConfigVoiceChannels = findUniqueChannels(voiceChannels);
+
+    client.modules['auto-delete'].uniqueChannels = uniqueConfigChannels.filter((channel) => {
+        const channelConfigObject = uniqueConfigVoiceChannels.find((voiceChannel) => voiceChannel.channelID === channel.channelID);
+        return !channelConfigObject;
+    });
+
+    for (const channel of client.modules['auto-delete'].uniqueChannels) {
         if (!channel.purgeOnStart) continue;
         const dcChannel = await client.channels.fetch(channel.channelID).catch(() => {
         });
@@ -20,3 +29,19 @@ module.exports.run = async function (client) {
         dcVoiceChannel.bulkDelete(await dcVoiceChannel.messages.fetch(), true);
     }
 };
+
+/**
+ * Finds and deletes duplicates in Array (Last Writer wins)
+ * @param {String} arrayToFilter Array of Channels
+ * @returns {Array} Filtered Array of Channels
+ * @private
+ */
+function findUniqueChannels(arrayToFilter) {
+    const uniqueConfigChannelIds = {};
+
+    for (let i = 0; i < arrayToFilter.length; i++) {
+        uniqueConfigChannelIds[arrayToFilter[i].channelID] = i;
+    }
+
+    return arrayToFilter.filter((channel, index) => uniqueConfigChannelIds[channel.channelID] === index);
+}
