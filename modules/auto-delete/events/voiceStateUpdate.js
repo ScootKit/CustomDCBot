c427d7a11a653d1b90684fb040ee277574d0f9f0
Torben Probst <torben.probst@caritas-altenoythe.de>
2022-08-04 16:02:32 +0200

    [auto-delete] added new event and feature functionality in-voice chats

---
+++ modules/auto-delete/events/voiceStateUpdate.js
@@ -0,0 +1,18 @@
+module.exports.run = async function (client, oldState) {
+    if (!client.botReadyAt) return;
+
+    const voiceChannels = client.configurations['auto-delete']['voice-channels'];
+
+    const channelConfigEntry = {...voiceChannels.find((vc) => oldState.channelId === vc.channelID)};
+    if (!channelConfigEntry.channelID) return;
+
+    const channel = await client.channels.fetch(channelConfigEntry.channelID);
+    if (!channel) {
+        return client.logger.error(`[auto-delete] ${localize('auto-delete', 'could-not-fetch-channel', {c: channel.channelID})}`);
+    }
+    if (channel.members.size) return;
+
+    setTimeout(async() => {
+        channel.bulkDelete(await channel.messages.fetch(), true).catch(() => {});
+    }, parseInt(channelConfigEntry.timeout * 1000 * 60));
+};
