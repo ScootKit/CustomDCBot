b93efd6d0538bd010f8bbd190ec3d89a25828899
Torben Probst <46575728+torbenprobst@users.noreply.github.com>
2022-09-09 15:10:20 +0200

    feat: auto-delete in voice-chat feature (#72)
    * added new config file to module config
    
    * fixed text description
    
    * added example config for voice-channel feature
    
    * added in-voice chat purgeOnStart functionality;
    
    * added filtering of channels (no duplicates in config); Added overwriting of voice-channels config
    
    * added reusing of botReady unique channels to reduce work load
    
    * [auto-delete] added new event and feature functionality in-voice chats
    
    * [auto-delete] added "could-not-fetch-messages" locale
    
    * [auto-delete] fixed missing import of localize function
    
    * [auto-delete] deleted unnecessary object cloning
    
    * [auto-delete] added catch functionality on channel.fetch
    
    * [auto-delete] fixed if statement to be more readable
    
    * [auto-delete] added channel message catch functionality
    
    * [auto-delete] added early exit on no messages in channel
    
    * [auto-delete] added now using variable for message parameter
    
    * [auto-delete] fixed unnecessary new line
    
    * [auto-delete] added channel message catch functionality + early return
    
    * [auto-delete] added usage of channelMessage variable in bulkDelete
    
    * [auto-delete] fixed if statement to be more readable
    
    * [auto-delete] added channel.messages catch functionality + early return
    
    * [auto-delete] added usage of channelMessage variable in Bulkdelete
    
    * [auto-delete] fixed order of if statements to fit logically
    
    * fixed eslint errors
    
    * [auto-delete] fixed logic errors and restructured code
    
    * fix(auto-delete): only parsing not int valus as int
    
    * fix(auto-delete): changed early return statement
    
    * fix(auto-delete): added check for Voice channel Type
    
    * feat(auto-delete:config): added more detailed bilingual description and names
    
    * fix(auto-delete): changed from "Zeit" to Timeout
    
    * feat(auto-delete): added locale entries for could-not-fetch-messages
    
    * fix(auto-delete): translated text to locale language
    
    Co-authored-by: Torben Probst <torben.probst@caritas-altenoythe.de>
    Co-authored-by: Torben Probst <dev@torben-probst.de>

--- modules/auto-delete/events/botReady.js
+++ modules/auto-delete/events/botReady.js
@@ -1,11 +1,60 @@
 const {localize} = require('../../../src/functions/localize');
 module.exports.run = async function (client) {
     const channels = client.configurations['auto-delete']['channels'];
-    for (const channel of channels) {
+    const voiceChannels = client.configurations['auto-delete']['voice-channels'];
+
+    const uniqueConfigChannels = findUniqueChannels(channels);
+    const uniqueConfigVoiceChannels = findUniqueChannels(voiceChannels);
+
+    client.modules['auto-delete'].uniqueChannels = uniqueConfigChannels.filter((channel) => {
+        const channelConfigObject = uniqueConfigVoiceChannels.find((voiceChannel) => voiceChannel.channelID === channel.channelID);
+        return !channelConfigObject;
+    });
+
+    for (const channel of client.modules['auto-delete'].uniqueChannels) {
         if (!channel.purgeOnStart) continue;
-        const dcChannel = await client.channels.fetch(channel.channelID).catch(() => {
-        });
+
+        const dcChannel = await client.channels.fetch(channel.channelID).catch(() => {});
         if (!dcChannel) return client.logger.error(`[auto-delete] ${localize('auto-delete', 'could-not-fetch-channel', {c: channel.channelID})}`);
-        dcChannel.bulkDelete((await dcChannel.messages.fetch()).filter(m => !m.pinned && m.deletable), true);
+
+        const channelMessages = await dcChannel.messages.fetch().catch(() => {});
+        if (!channelMessages) {
+            return client.logger.error(`[auto-delete] ${localize('auto-delete', 'could-not-fetch-messages', {c: channel.channelID})}`);
+        }
+        if (channelMessages.size === 0) continue;
+
+        dcChannel.bulkDelete(channelMessages.filter(m => !m.pinned && m.deletable), true);
+    }
+
+    for (const voiceChannel of uniqueConfigVoiceChannels) {
+        if (!voiceChannel.purgeOnStart) continue;
+
+        const dcVoiceChannel = await client.channels.fetch(voiceChannel.channelID).catch(() => {});
+        if (!dcVoiceChannel) return client.logger.error(`[auto-delete] ${localize('auto-delete', 'could-not-fetch-channel', {c: voiceChannel.channelID})}`);
+        if (dcVoiceChannel.members.size > 0) continue;
+
+        const channelMessages = await dcVoiceChannel.messages.fetch().catch(() => {});
+        if (!channelMessages) {
+            return client.logger.error(`[auto-delete] ${localize('auto-delete', 'could-not-fetch-messages', {c: voiceChannel.channelID})}`);
+        }
+        if (channelMessages.size === 0) continue;
+
+        dcVoiceChannel.bulkDelete(channelMessages, true);
     }
 };
+
+/**
+ * Finds and deletes duplicates in Array (Last Writer wins)
+ * @param {String} arrayToFilter Array of Channels
+ * @returns {Array} Filtered Array of Channels
+ * @private
+ */
+function findUniqueChannels(arrayToFilter) {
+    const uniqueConfigChannelIds = {};
+
+    for (let i = 0; i < arrayToFilter.length; i++) {
+        uniqueConfigChannelIds[arrayToFilter[i].channelID] = i;
+    }
+
+    return arrayToFilter.filter((channel, index) => uniqueConfigChannelIds[channel.channelID] === index);
+}
--- modules/auto-delete/events/messageCreate.js
+++ modules/auto-delete/events/messageCreate.js
@@ -3,9 +3,9 @@ module.exports.run = async function (client, msg) {
     if (!msg.guild) return;
     if (msg.guild.id !== client.guildID) return;
     if (!msg.member) return;
+    if (!client.modules['auto-delete'].uniqueChannels) return;
 
-    const channels = client.configurations['auto-delete']['channels'];
-    const channel = channels.find(c => c.channelID === msg.channel.id);
+    const channel = client.modules['auto-delete'].uniqueChannels.find(c => c.channelID === msg.channel.id);
     if (!channel) return;
     setTimeout(() => {
         if (msg.deletable && !msg.pinned) msg.delete().catch(() => {
