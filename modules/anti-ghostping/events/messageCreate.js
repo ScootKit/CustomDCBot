332304a9ad5cf75515d019e9c5a0a3e0e4e71380
Martin <box152535@gmail.com>
2022-11-20 22:15:50 +0100

    Anti-Ghost-Ping Verbesserungen
    Rollen Whitelist - User, die Rollen besitzen die auf der Whitelist sind werden vom anti-ghost-ping ignoriert

--- modules/anti-ghostping/events/messageCreate.js
+++ modules/anti-ghostping/events/messageCreate.js
@@ -5,6 +5,12 @@ module.exports.run = async function (client, msg) {
     if (msg.guild.id !== client.config.guildID) return;
     const moduleConfig = client.configurations['anti-ghostping']['config'];
     if (moduleConfig.ignoredChannels.includes(msg.channel.id)) return;
+    let whitelisted = false;
+    moduleConfig.ignoredRoles.forEach(r => {
+        if (msg.member.roles.cache.get(r)) whitelisted = true;
+    });
+    if (whitelisted) return;
+    if (moduleConfig.ignoredRoles.includes(msg.channel.id)) return;
     if (msg.mentions.members.size !== 0) msgsWithMention[msg.id] = msg;
     setTimeout(() => {
         msgsWithMention[msg.id] = null;
