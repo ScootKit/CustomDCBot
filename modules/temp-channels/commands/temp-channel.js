8e72bcc21f6b7a4bf84a8f702d75fcc49fde9bf8
hfgd <46094961+hfgd123@users.noreply.github.com>
2022-12-26 22:45:25 +0100

    Added button-based settings for temp channels (#96)
    * Added button-based settings for temp channels
    
    * I love you too ESLint
    
    * Fixed stuff Simon told me to fix
    
    * Fixed stuff again (i hope)
    
    * fixed indentation
    
    * spaces...
    
    * Added a space
    
    * Now limit number checking
    
    * Stole the module 🔪
    
    * Added some locales
    
    * Partially added translations; will continue at home
    
    * added the rest of the translations

--- modules/temp-channels/commands/temp-channel.js
+++ modules/temp-channels/commands/temp-channel.js
@@ -1,7 +1,7 @@
 const {localize} = require('../../../src/functions/localize');
 const {client} = require('../../../main');
 const {Op} = require('sequelize');
-const {embedType} = require('../../../src/functions/helpers');
+const {channelMode, userAdd, userRemove, usersList, channelEdit} = require('../channel-settings');
 
 module.exports.beforeSubcommand = async function (interaction) {
     await interaction.deferReply({ephemeral: true});
@@ -23,168 +23,23 @@ module.exports.beforeSubcommand = async function (interaction) {
 module.exports.subcommands = {
     'mode': async function (interaction) {
         if (interaction.cancel) return;
-        const moduleConfig = interaction.client.configurations['temp-channels']['config'];
-        const vc = await client.models['temp-channels']['TempChannel'].findOne({
-            where: {
-                [Op.and]: [
-                    {id: interaction.member.voice.channelId},
-                    {creatorID: interaction.member.id}
-                ]
-            }
-        });
-        const allowedUsers = vc.allowedUsers.split(',');
-        const vchann = interaction.guild.channels.cache.get(vc.id);
-        if (interaction.options.getBoolean('public')) {
-            await vchann.lockPermissions;
-            await vchann.permissionOverwrites.delete(vchann.guild.roles.everyone);
-            await interaction.editReply(await embedType(moduleConfig['modeSwitched'], {'%mode%': 'public'}, {ephemeral: true}));
-        } else if (!interaction.options.getBoolean('public')) {
-            await vchann.lockPermissions;
-            await vchann.permissionOverwrites.create(vchann.guild.roles.everyone, {'CONNECT': false});
-            if (allowedUsers.at(0) !== '') {
-                for (const user of allowedUsers) {
-                    await vchann.permissionOverwrites.create(interaction.guild.members.cache.get(user), {'CONNECT': true});
-                }
-            }
-            interaction.editReply(await embedType(moduleConfig['modeSwitched'], {'%mode%': 'private'}, {ephemeral: true}));
-        }
-        vc.isPublic = interaction.options.getBoolean('public');
-        await vc.save;
+        await channelMode(interaction, 'command');
     },
     'add-user': async function (interaction) {
         if (interaction.cancel) return;
-        const moduleConfig = interaction.client.configurations['temp-channels']['config'];
-        const vc = await client.models['temp-channels']['TempChannel'].findOne({
-            where: {
-                [Op.and]: [
-                    {id: interaction.member.voice.channelId},
-                    {creatorID: interaction.member.id}
-                ]
-            }
-        });
-        let allowedUsers = vc.allowedUsers;
-        if (allowedUsers === '') {
-            allowedUsers = interaction.options.getUser('user').id;
-        } else {
-            allowedUsers = allowedUsers + ',' + interaction.options.getUser('user').id;
-        }
-        vc.allowedUsers = allowedUsers;
-        await vc.save();
-        const vchann = interaction.guild.channels.cache.get(vc.id);
-        if (!await vchann.permissionsFor(vchann.guild.roles.everyone).has('CONNECT')) {
-            await vchann.permissionOverwrites.create(interaction.options.getUser('user'), {'CONNECT': true});
-        }
-        interaction.editReply(await embedType(moduleConfig['userAdded'], {'%user%': interaction.options.getUser('user').tag}, {ephemeral: true}));
+        await userAdd(interaction, 'command');
     },
     'remove-user': async function (interaction) {
         if (interaction.cancel) return;
-        const moduleConfig = interaction.client.configurations['temp-channels']['config'];
-        const vc = await client.models['temp-channels']['TempChannel'].findOne({
-            where: {
-                [Op.and]: [
-                    {id: interaction.member.voice.channelId},
-                    {creatorID: interaction.member.id}
-                ]
-            }
-        });
-        let allowedUsers = vc.allowedUsers.split(',');
-        const user = interaction.options.getUser('user').id;
-        allowedUsers = allowedUsers.filter((e => e !== user));
-        allowedUsers = allowedUsers.toString();
-        vc.allowedUsers = allowedUsers;
-        await vc.save();
-        const vchann = interaction.guild.channels.cache.get(vc.id);
-        try {
-            await vchann.permissionOverwrites.delete(interaction.options.getUser('user'));
-        } catch (e) {
-            console.log(e);
-        }
-        const usr = interaction.guild.members.cache.get(interaction.options.getUser('user').id);
-        if (usr.voice.channelId === vc.id) {
-            try {
-                await usr.voice.disconnect();
-            } catch (e) {
-                interaction.editReply(localize('temp-channels', 'no-disconnect'));
-                return;
-            }
-        }
-        interaction.editReply(await embedType(moduleConfig['userRemoved'], {'%user%': interaction.options.getUser('user').tag}, {ephemeral: true}));
+        await userRemove(interaction, 'command');
     },
     'list-users': async function (interaction) {
         if (interaction.cancel) return;
-        const moduleConfig = interaction.client.configurations['temp-channels']['config'];
-        const vc = await client.models['temp-channels']['TempChannel'].findOne({
-            where: {
-                [Op.and]: [
-                    {id: interaction.member.voice.channelId},
-                    {creatorID: interaction.member.id}
-                ]
-            }
-        });
-        const allowedUsersArray = vc.allowedUsers.split(',');
-        let allowedUsers = '';
-        for (const user of allowedUsersArray) {
-            allowedUsers = allowedUsers + '\n • <@' + user + '>';
-        }
-        if (allowedUsersArray.at(0) === '') {
-            interaction.editReply(localize('temp-channels', 'no-added-user'));
-            return;
-        }
-        interaction.editReply(moduleConfig['listUsers'] + ' ' + allowedUsers);
+        await usersList(interaction, 'command');
     },
     'edit': async function (interaction) {
         if (interaction.cancel) return;
-        const moduleConfig = interaction.client.configurations['temp-channels']['config'];
-        const vc = await client.models['temp-channels']['TempChannel'].findOne({
-            where: {
-                [Op.and]: [
-                    {id: interaction.member.voice.channelId},
-                    {creatorID: interaction.member.id}
-                ]
-            }
-        });
-        const vchann = interaction.guild.channels.cache.get(vc.id);
-        let edited = 0;
-        let vcNsfw;
-        let vcBitrate;
-        let vcLimit;
-        let vcName;
-
-        if (interaction.options.getInteger('user-limit') >= 0) {
-            if (interaction.options.getInteger('user-limit') < 0 || interaction.options.getInteger('user-limit') > 99) {
-                interaction.editReply(localize('temp-channels', 'edit-error'));
-                return;
-            }
-            vcLimit = interaction.options.getInteger('user-limit');
-            edited++;
-        } else vcLimit = vchann.userLimit;
-        if (interaction.options.getInteger('bitrate')) {
-            if (interaction.options.getInteger('bitrate') <= 8000 || interaction.options.getInteger('bitrate') >= interaction.guild.maximumBitrate) {
-                interaction.editReply(localize('temp-channels', 'edit-error'));
-                return;
-            }
-            vcBitrate = interaction.options.getInteger('bitrate');
-            edited++;
-        } else vcBitrate = vchann.bitrate;
-        if (interaction.options.getString('name')) {
-            vcName = interaction.options.getString('name');
-            edited++;
-        } else vcName = vchann.name;
-        if (interaction.options.getBoolean('nsfw')) {
-            vcNsfw = interaction.options.getBoolean('nsfw');
-            edited++;
-        } else vcNsfw = vchann.nsfw;
-
-        if (edited !== 0) {
-            interaction.editReply(await embedType(moduleConfig['channelEdited'], {}, {ephemeral: true}));
-            try {
-                vchann.edit({userLimit: vcLimit, nsfw: vcNsfw, name: vcName, bitrate: vcBitrate});
-            } catch (e) {
-                interaction.editReply(localize('temp-channels', 'edit-error'));
-            }
-        } else {
-            interaction.editReply(localize('temp-channels', 'nothing-changed'));
-        }
+        await channelEdit(interaction, 'command');
     }
 };
 
---
+++ modules/temp-channels/events/interactionCreate.js
@@ -0,0 +1,173 @@
+const {MessageActionRow, Modal, TextInputComponent} = require('discord.js');
+const {usersList, channelMode, userAdd, userRemove, channelEdit} = require('../channel-settings');
+const {localize} = require('../../../src/functions/localize');
+const {Op} = require('sequelize');
+
+module.exports.run = async function (client, interaction) {
+    if (!client.botReadyAt) return;
+    if (interaction.guild.id !== client.config.guildID) return;
+    if (interaction.isButton()) {
+        const vc = await client.models['temp-channels']['TempChannel'].findOne({
+            where: {
+                [Op.and]: [
+                    {id: interaction.member.voice.channelId},
+                    {creatorID: interaction.member.id}
+                ]
+            }
+        });
+
+
+        if (interaction.customId === 'tempc-add') {
+            if (!vc) {
+                interaction.reply({ ephemeral: true,
+                    content: interaction.client.configurations['temp-channels']['config']['notInChannel']});
+                return;
+            }
+            const modal = new Modal()
+                .setCustomId('tempc-add-modal')
+                .setTitle(localize('temp-channels', 'add-modal-title'));
+            const userInput = new TextInputComponent()
+                .setCustomId('add-modal-input')
+                .setLabel(localize('temp-channels', 'add-modal-prompt'))
+                .setStyle('SHORT')
+                .setPlaceholder('User#1234');
+            const actionRow = new MessageActionRow().addComponents(userInput);
+            modal.addComponents(actionRow);
+            await interaction.showModal(modal);
+        }
+        if (interaction.customId === 'tempc-remove') {
+            if (!vc) {
+                interaction.reply({ ephemeral: true,
+                    content: interaction.client.configurations['temp-channels']['config']['notInChannel']});
+                return;
+            }
+            const modal = new Modal()
+                .setCustomId('tempc-remove-modal')
+                .setTitle(localize('temp-channels', 'remove-modal-title'));
+            const userInput = new TextInputComponent()
+                .setCustomId('remove-modal-input')
+                .setLabel(localize('temp-channels', 'remove-modal-prompt'))
+                .setStyle('SHORT')
+                .setPlaceholder('User#1234');
+            const actionRow = new MessageActionRow().addComponents(userInput);
+            modal.addComponents(actionRow);
+            await interaction.showModal(modal);
+        }
+        if (interaction.customId === 'tempc-list') {
+            if (!vc) {
+                interaction.reply({ ephemeral: true,
+                    content: interaction.client.configurations['temp-channels']['config']['notInChannel']});
+                return;
+            }
+            await interaction.deferReply({ephemeral: true});
+            await usersList(interaction);
+        }
+        if (interaction.customId === 'tempc-private') {
+            if (!vc) {
+                interaction.reply({ ephemeral: true,
+                    content: interaction.client.configurations['temp-channels']['config']['notInChannel']});
+                return;
+            }
+            await interaction.deferReply({ephemeral: true});
+            await channelMode(interaction, 'buttonPrivate');
+        }
+        if (interaction.customId === 'tempc-public') {
+            if (!vc) {
+                interaction.reply({ ephemeral: true,
+                    content: interaction.client.configurations['temp-channels']['config']['notInChannel']});
+                return;
+            }
+            await interaction.deferReply({ephemeral: true});
+            await channelMode(interaction, 'buttonPublic');
+        }
+        if (interaction.customId === 'tempc-edit') {
+            if (!vc) {
+                interaction.reply({ ephemeral: true,
+                    content: interaction.client.configurations['temp-channels']['config']['notInChannel']});
+                return;
+            }
+            const vchann = interaction.guild.channels.cache.get(vc.id);
+            const modal = new Modal()
+                .setCustomId('tempc-edit-modal')
+                .setTitle(localize('temp-channels', 'edit-modal-title'));
+            const nsfwInput = new TextInputComponent()
+                .setCustomId('edit-modal-nsfw-input')
+                .setLabel(localize('temp-channels', 'edit-modal-nsfw-prompt'))
+                .setRequired(true)
+                .setStyle('SHORT')
+                .setPlaceholder(localize('temp-channels', 'edit-modal-nsfw-placeholder'))
+                .setValue(vchann.nsfw.toString());
+
+
+            const bitrateInput = new TextInputComponent()
+                .setCustomId('edit-modal-bitrate-input')
+                .setLabel(localize('temp-channels', 'edit-modal-bitrate-prompt'))
+                .setRequired(true)
+                .setStyle('SHORT')
+                .setPlaceholder(localize('temp-channels', 'edit-modal-bitrate-placeholder'))
+                .setValue(vchann.bitrate.toString());
+
+            const limitInput = new TextInputComponent()
+                .setCustomId('edit-modal-limit-input')
+                .setLabel(localize('temp-channels', 'edit-modal-limit-prompt'))
+                .setRequired(true)
+                .setStyle('SHORT')
+                .setPlaceholder(localize('temp-channels', 'edit-modal-limit-placeholder'))
+                .setValue(vchann.userLimit.toString());
+
+            const nameInput = new TextInputComponent()
+                .setCustomId('edit-modal-name-input')
+                .setLabel(localize('temp-channels', 'edit-modal-name-prompt'))
+                .setRequired(true)
+                .setStyle('SHORT')
+                .setPlaceholder(localize('temp-channels', 'edit-modal-name-placeholder'))
+                .setValue(vchann.name);
+
+            const nsfwRow = new MessageActionRow().addComponents(nsfwInput);
+            const bitrateRow = new MessageActionRow().addComponents(bitrateInput);
+            const limitRow = new MessageActionRow().addComponents(limitInput);
+            const nameRow = new MessageActionRow().addComponents(nameInput);
+            modal.addComponents(bitrateRow);
+            modal.addComponents(limitRow);
+            modal.addComponents(nameRow);
+            modal.addComponents(nsfwRow);
+            await interaction.showModal(modal);
+        }
+    } else if (interaction.isModalSubmit()) {
+        const vc = await client.models['temp-channels']['TempChannel'].findOne({
+            where: {
+                [Op.and]: [
+                    {id: interaction.member.voice.channelId},
+                    {creatorID: interaction.member.id}
+                ]
+            }
+        });
+        if (interaction.customId === 'tempc-add-modal') {
+            if (!vc) {
+                interaction.reply({ ephemeral: true,
+                    content: interaction.client.configurations['temp-channels']['config']['notInChannel']});
+                return;
+            }
+            await interaction.deferReply({ephemeral: true});
+            await userAdd(interaction, 'modal');
+        }
+        if (interaction.customId === 'tempc-remove-modal') {
+            if (!vc) {
+                interaction.reply({ ephemeral: true,
+                    content: interaction.client.configurations['temp-channels']['config']['notInChannel']});
+                return;
+            }
+            await interaction.deferReply({ephemeral: true});
+            await userRemove(interaction, 'modal');
+        }
+        if (interaction.customId === 'tempc-edit-modal') {
+            if (!vc) {
+                interaction.reply({ ephemeral: true,
+                    content: interaction.client.configurations['temp-channels']['config']['notInChannel']});
+                return;
+            }
+            await interaction.deferReply({ephemeral: true});
+            await channelEdit(interaction, 'modal');
+        }
+    }
+};
