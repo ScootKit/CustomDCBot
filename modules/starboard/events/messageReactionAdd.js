df61ccd33fac8e6d69f2d93428925968cadc6811
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-09-21 21:52:27 +0200

    More starboard code

--- modules/starboard/events/messageReactionAdd.js
+++ modules/starboard/events/messageReactionAdd.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, msgReaction) => {
 
     const starConfig = client.configurations['starboard']['config'];
     if (!starConfig || (starConfig.emoji !== msgReaction.emoji.name && starConfig.emoji !== msgReaction.emoji.id)) return;
-	if (isNaN(starConfig.minStars)) return disableModule('starboard', localize('starboard', 'invalid-minstars', {stars: starConfig.minStars}));
+    if (isNaN(starConfig.minStars)) return disableModule('starboard', localize('starboard', 'invalid-minstars', {stars: starConfig.minStars}));
 
     const starMsg = await client.models['starboard']['StarMsg'].findOne({
         where: {
@@ -20,19 +20,19 @@ module.exports.run = async (client, msgReaction) => {
     if (!channel) return disableModule('starboard', localize('partner-list', 'channel-not-found', {c: starConfig.channelId}));
     if (!channel || (msg.channel.nsfw && !channel.nsfw) || starConfig.excludedChannels.includes(msg.channel.id) || starConfig.excludedRoles.some(r => msg.member.roles.cache.has(r))) return;
 
-	let reactioncount = msgReaction.count;
-	if (!starConfig.selfStar && msgReaction.users.cache.has(msg.author.id)) reactioncount--;
-	if (reactioncount < starConfig.minStars) return;
+    let reactioncount = msgReaction.count;
+    if (!starConfig.selfStar && msgReaction.users.cache.has(msg.author.id)) reactioncount--;
+    if (reactioncount < starConfig.minStars) return;
 
-    const starboardMsg = starMsg ? await channel.messages.fetch(starMsg.starMsg).catch(() => {}) : undefined;
+    const starboardMsg = starMsg ? await channel.messages.fetch(starMsg.starMsg).catch(() => {}) : null;
     const generatedMsg = await embedTypeV2(starConfig.message, {
-        "%stars%": msgReaction.count,
-        "%content%": msg.content,
-        "%username%": msg.author.username,
-        "%nickname%": msg.member ? msg.member.displayName : msg.author.username,
-        "%channelname%": msg.channel.name,
-        "%channellink%": msg.channel.url,
-        "%link%": msg.url
+        '%stars%': msgReaction.count,
+        '%content%': msg.content,
+        '%username%': msg.author.username,
+        '%nickname%': msg.member ? msg.member.displayName : msg.author.username,
+        '%channelname%': msg.channel.name,
+        '%channellink%': msg.channel.url,
+        '%link%': msg.url
     });
 
     if (starboardMsg) starboardMsg.edit(generatedMsg);
--- modules/starboard/events/messageReactionRemove.js
+++ modules/starboard/events/messageReactionRemove.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, msgReaction) => {
 
     const starConfig = client.configurations['starboard']['config'];
     if (!starConfig || (starConfig.emoji !== msgReaction.emoji.name && starConfig.emoji !== msgReaction.emoji.id)) return;
-	if (isNaN(starConfig.minStars)) return disableModule('starboard', localize('starboard', 'invalid-minstars', {stars: starConfig.minStars}));
+    if (isNaN(starConfig.minStars)) return disableModule('starboard', localize('starboard', 'invalid-minstars', {stars: starConfig.minStars}));
 
     const starMsg = await client.models['starboard']['StarMsg'].findOne({
         where: {
@@ -20,28 +20,28 @@ module.exports.run = async (client, msgReaction) => {
     if (!channel) return disableModule('starboard', localize('partner-list', 'channel-not-found', {c: starConfig.channelId}));
     if (!channel || (msg.channel.nsfw && !channel.nsfw) || starConfig.excludedChannels.includes(msg.channel.id) || starConfig.excludedRoles.some(r => msg.member.roles.cache.has(r))) return;
 
-	let reactioncount = msgReaction.count;
-	if (!starConfig.selfStar && msgReaction.users.cache.has(msg.author.id)) reactioncount--;
+    let reactioncount = msgReaction.count;
+    if (!starConfig.selfStar && msgReaction.users.cache.has(msg.author.id)) reactioncount--;
 
-    const starboardMsg = starMsg ? await channel.messages.fetch(starMsg.starMsg).catch(() => {}) : undefined;
-	if (reactioncount < starConfig.minStars) {
-		if (starboardMsg) starboardMsg.delete();
-		client.models['starboard']['StarMsg'].destroy({
-			where: {
-				msgId: msg.id
-			}
-		});
-		return;
-	}
+    const starboardMsg = starMsg ? await channel.messages.fetch(starMsg.starMsg).catch(() => {}) : null;
+    if (reactioncount < starConfig.minStars) {
+        if (starboardMsg) starboardMsg.delete();
+        client.models['starboard']['StarMsg'].destroy({
+            where: {
+                msgId: msg.id
+            }
+        });
+        return;
+    }
 
     const generatedMsg = await embedTypeV2(starConfig.message, {
-        "%stars%": msgReaction.count,
-        "%content%": msg.content,
-        "%username%": msg.author.username,
-        "%nickname%": msg.member ? msg.member.displayName : msg.author.username,
-        "%channelname%": msg.channel.name,
-        "%channellink%": msg.channel.url,
-        "%link%": msg.url
+        '%stars%': msgReaction.count,
+        '%content%': msg.content,
+        '%username%': msg.author.username,
+        '%nickname%': msg.member ? msg.member.displayName : msg.author.username,
+        '%channelname%': msg.channel.name,
+        '%channellink%': msg.channel.url,
+        '%link%': msg.url
     });
 
     if (starboardMsg) starboardMsg.edit(generatedMsg);
