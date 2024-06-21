f1edd6174369a3c56bb065a3b6cffb97c616c97d
hfgd <jan-lunz@web.de>
2024-06-22 01:16:45 +0200

    removed debug logging no longer WIP

--- modules/nicknames/events/botReady.js
+++ modules/nicknames/events/botReady.js
@@ -1,7 +1,6 @@
 const {renameMember} = require('../renameMember');
 
 module.exports.run = async function (client) {
-    console.log("EEEE")
     for (const member of client.guild.members.cache.values()) {
         await renameMember(client, member);
     }
