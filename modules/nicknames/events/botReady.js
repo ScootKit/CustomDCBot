812811ee2fafcb37a8966a3756dfdfdcea7464c0
hfgd <46094961+hfgd123@users.noreply.github.com>
2024-06-22 01:14:12 +0200

    Update modules/nicknames/events/botReady.js
    Co-authored-by: Simon <mail@scderox.de>

--- modules/nicknames/events/botReady.js
+++ modules/nicknames/events/botReady.js
@@ -2,7 +2,7 @@ const {renameMember} = require('../renameMember');
 
 module.exports.run = async function (client) {
     console.log("EEEE")
-    for (const member of client.guild.members.cache) {
+    for (const member of client.guild.members.cache.values()) {
         await renameMember(client, member);
     }
 
