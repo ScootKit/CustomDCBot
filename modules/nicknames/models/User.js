8618d2675b2014d9fb778316d7277f220f4d00ac
hfgd <46094961+hfgd123@users.noreply.github.com>
2024-06-22 18:04:22 +0200

    added nickname module (#142)
    * added nickname module
    
    * small changes
    - simplified hoistrole variable
    - not comparing to null
    - un-rawed the database
    - truncating long names
    
    * more small changes
    
    * Update modules/nickname/module.json
    
    Co-authored-by: Simon <mail@scderox.de>
    
    * did more
    - renamed things
    - removed legacy stuff
    - using highest role instead of hoisted role now
    
    * now checking for multiple roles
    
    * Update guildMemberUpdate.js
    
    Removed disabled legacy code
    
    * Update modules/nicknames/events/guildMemberUpdate.js
    
    Co-authored-by: Simon <mail@scderox.de>
    
    * Update modules/nicknames/models/User.js
    
    Co-authored-by: Simon <mail@scderox.de>
    
    * !!WIP!! sync prefix on startup
    
    * Update modules/nicknames/events/botReady.js
    
    Co-authored-by: Simon <mail@scderox.de>
    
    * removed debug logging
    no longer WIP
    
    * Update modules/nicknames/renameMember.js
    
    Co-authored-by: Simon <mail@scderox.de>
    
    * added more readable logging
    - checking for unnecessary cases
    
    * prefix
    
    * Update modules/nicknames/events/guildMemberUpdate.js
    
    Co-authored-by: Simon <mail@scderox.de>
    
    * Update modules/nicknames/renameMember.js
    
    Co-authored-by: Simon <mail@scderox.de>
    
    * Update modules/nicknames/renameMember.js
    
    Co-authored-by: Simon <mail@scderox.de>
    
    * Update modules/nicknames/renameMember.js
    
    Co-authored-by: Simon <mail@scderox.de>
    
    * Update modules/nicknames/renameMember.js
    
    Co-authored-by: Simon <mail@scderox.de>
    
    * Update modules/nicknames/renameMember.js
    
    Co-authored-by: Simon <mail@scderox.de>
    
    ---------
    
    Co-authored-by: Simon <mail@scderox.de>

---
+++ modules/nicknames/models/User.js
@@ -0,0 +1,22 @@
+const { DataTypes, Model } = require('sequelize');
+
+module.exports = class User extends Model {
+    static init(sequelize) {
+        return super.init({
+            userID: {
+                type: DataTypes.STRING,
+                primaryKey: true
+            },
+            nickname: DataTypes.JSON
+        }, {
+            tableName: 'nicknames_User',
+            timestamps: true,
+            sequelize
+        });
+    }
+};
+
+module.exports.config = {
+    'name': 'User',
+    'module': 'nicknames'
+};
