209e9e855d92f775c28bb8f0106585a08bf88c63
hfgd <46094961+hfgd123@users.noreply.github.com>
2024-06-22 17:14:02 +0200

    Update modules/nicknames/events/guildMemberUpdate.js
    Co-authored-by: Simon <mail@scderox.de>

--- modules/nicknames/events/guildMemberUpdate.js
+++ modules/nicknames/events/guildMemberUpdate.js
@@ -4,7 +4,7 @@ module.exports.run = async function (client, oldGuildMember, newGuildMember) {
 
     if (!client.botReadyAt) return;
     if (newGuildMember.guild.id !== client.guild.id) return;
-    if (newGuildMember.nickname === oldGuildMember.nickname && newGuildMember.roles.cache.size === oldGuildMember.guild.me.roles.cache.size) return;
+    if (newGuildMember.nickname === oldGuildMember.nickname && newGuildMember.roles.cache.size === oldGuildMember.roles.cache.size) return;
 
     await renameMember(client, newGuildMember);
 
